10da298df30f77b60c7fc4f7aae41e3980d96e00
Guilherme Ramos da Silva <115495679+LucaJacentink@users.noreply.github.com>
2023-01-30 10:40:04 -0300

    NC Rodando

--- src/classifiers/nc_classifier.py
+++ src/classifiers/nc_classifier.py
@@ -10,44 +10,50 @@ class NearestCentroidClassifier(ClassifierInterface):
         self.tuplas=[]
         self.Centroides=[]
         self.classes=[]
+        self.vetores=[]
+        self.tuplas_test=[]
         
     def train(self, train_dataset: DatasetInterface) -> None:
-        self.classes=[]
-        vetores=[]
-        self.Centroides=[]
         for i in range(train_dataset.size()):
             self.tuplas.append(train_dataset.get(i))
             if (self.tuplas[i][1]) not in self.classes:
                self.classes.append(self.tuplas[i][1])
         soma=0     
         for i in range(len(self.classes)):
-            vetores.append([])
+            self.vetores.append([])
             for j in range(train_dataset.size()):
                 if (self.tuplas[j][1])==self.classes[i]:
-                    vetores[i].append(self.tuplas[j][0])
+                    self.vetores[i].append(self.tuplas[j][0])
         for i in range(len(self.classes)):
             self.Centroides.append([])
-            for j in range (len(vetores[0][0])):
-                for k in range (len(vetores[i])):
-                    soma+=vetores[i][k][j]
-                media=soma/len(vetores[i][0])
+            for j in range (len(self.vetores[0][0])):
+                for k in range (len(self.vetores[i])):
+                    soma+=self.vetores[i][k][j]
+                media=soma/len(self.vetores[i][0])
                 self.Centroides[i].append(media)
                 soma=0
         print(self.Centroides)
-           
+        print(self.classes)
     def predict(self, test_dataset: DatasetInterface) -> List[str]:
         predicted_classes=[]
-        menor_dist=float('inf')
-        indice_menor_dist=2
+        distancias=[]
+
+
+        soma=0
         for i in range(test_dataset.size()):
-                for j in range(len(self.classes)):
-                        distancia=dist(self.tuplas[i][0],self.Centroides[j])
-                        if distancia<=menor_dist:
-                            menor_dist=distancia
-                            indice_menor_dist=j
-                predicted_classes.append(self.classes[indice_menor_dist])
-        print(predicted_classes)
-                
+            self.tuplas_test.append(test_dataset.get(i))
+        soma_dist=0
+        for i in range(test_dataset.size()):       
+            for j in range(len(self.Centroides)):      
+                for k in range(len(self.Centroides[j])):
+                        soma=dist(self.tuplas_test[i][0],self.Centroides[j])
+                distancias.append([soma, self.classes[j]])
+            distancias.sort(key=lambda x:x[0])
+            menor_dist=distancias[0]
+            predicted_classes.append(menor_dist[1])
+            print(distancias)
+            distancias.clear()
+        print(predicted_classes)   
         return predicted_classes
 
 
