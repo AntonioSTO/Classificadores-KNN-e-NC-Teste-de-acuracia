5914f4616cb0a31912983ea8ef5476eb16ac28fc
Guilherme Ramos da Silva <115495679+LucaJacentink@users.noreply.github.com>
2023-01-30 09:26:36 -0300

    NC ta quase la

--- src/classifiers/nc_classifier.py
+++ src/classifiers/nc_classifier.py
@@ -19,18 +19,22 @@ class NearestCentroidClassifier(ClassifierInterface):
             self.tuplas.append(train_dataset.get(i))
             if (self.tuplas[i][1]) not in self.classes:
                self.classes.append(self.tuplas[i][1])
-               
+        soma=0     
         for i in range(len(self.classes)):
-            for j in range(len(self.tuplas)):
+            vetores.append([])
+            for j in range(train_dataset.size()):
                 if (self.tuplas[j][1])==self.classes[i]:
-                    vetores.append(self.tuplas[j][0])
-                    print(self.classes[i])
-        
-            centro=np.mean(vetores, axis=0)
-            print(centro)
-            print(len(vetores))
-            self.Centroides.append(centro)
-            vetores.clear()
+                    vetores[i].append(self.tuplas[j][0])
+        for i in range(len(self.classes)):
+            self.Centroides.append([])
+            for j in range (len(vetores[0][0])):
+                for k in range (len(vetores[i])):
+                    soma+=vetores[i][k][j]
+                media=soma/len(vetores[i][0])
+                self.Centroides[i].append(media)
+                soma=0
+        print(self.Centroides)
+           
     def predict(self, test_dataset: DatasetInterface) -> List[str]:
         predicted_classes=[]
         menor_dist=float('inf')
